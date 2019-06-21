a60c939fb70737f31043086eb822a8c0d38f1fc3
Pallavi-Borse <51350021+Pallavi-Borse@users.noreply.github.com>
2019-06-21 14:48:38 +0530

    Update GeoLocation.js

--- GeoLocation.js
+++ GeoLocation.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'GeoLocation', 'coolMethod');
+    exec(success, error, 'GeoLocation', 'getLocation');
 };
