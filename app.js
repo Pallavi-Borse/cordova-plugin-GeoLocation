47cbccfcb119655cb957732517c7905cc89de73c
DB Mac <dbmac@MacBook-Pro.local>
2019-06-21 12:21:34 +0200

    GPS tracker issue resolved...

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
-  function success(latitude,longitude){
-            debugger;
-            alert("lat" + latitude+"Long"+longitude);
-        };
-        setTimeout( function(){
-            debugger;
-            Cordova.exec(success, null, "GeoLocation", "getLocation");
+//   function success(latitude,longitude){
+//             debugger;
+//             alert("lat" + latitude+"Long"+longitude);
+//         };
+//         setTimeout( function(){
+//             debugger;
+//             Cordova.exec(success, null, "GeoLocation", "getLoc");
            
-        }, 10000); 
+//         }, 10000); 
 
---
+++ src/android/GeoLocation.java
@@ -0,0 +1,29 @@
+package deven.geolocation;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+
+public class GeoLocation extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        GPSTracker gps = new GPSTracker(this.cordova.getActivity().getApplicationContext());
+        if (action.equals("getLocation")) {
+            if (gps.canGetLocation()) {
+                double latitude = gps.getLatitude();
+                double longitude = gps.getLongitude();
+                //double latitude = 18.23564;
+                //double longitude = 73.12566;
+                callbackContext.success("Lat" + latitude + ", Long:" + longitude);
+                return true;
+            } else {
+                gps.showSettingsAlert();
+            }
+        }
+        return false;
+    }
+}
